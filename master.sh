e0127a7eac2315fbec078570e6f5d12236c0e807
vasev85 <vasev85@gmail.com>
2022-10-28 12:22:22 +0300

    Delete master.sh

--- master.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker exec -it replication-master mysql -u root -p@H9p3v6c -e CREATE USER 'ssl_test'@'%' IDENTIFIED BY '123' REQUIRE SSL;
-docker exec -it replication-master mysql -u root -p@H9p3v6c -e GRANT REPLICATION SLAVE ON *.* TO 'ssl_test'@'%';
-docker exec -it replication-master mysqldump -u root -p@H9p3v6c --all-databases --master-data |gzip > /etc/mysql/dump.sql.gz 
-
-docker exec -it replication-master ' /bin/bash | mysqldump -u root -p@H9p3v6c  --all-databases --source-data  | gzip > /etc/cert/mysql/123' 
